0d73493f75dd0c45f869c5a7ac38c208f7c6ea21
loluxdev <dev@lolux.fun>
2024-03-14 23:26:49 +0100

    improve meta tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    particlesJS.load('particles-js', '/assets/js/particles.json', function () {
-        console.log('Particles.js loaded...');
-    });
-});
-
 let currentMemeIndex = 0;
 let memeData = null;
 
 function fetchMemeData() {
-    fetch(' https://meme-api.com/gimme/20')
+    fetch(' https://meme-api.com/gimme/50')
         .then(response => response.json())
         .then(data => {
             memeData = data;
@@ -22,14 +16,35 @@ function fetchMemeData() {
 function updateMemePreview() {
     const memeElement = document.getElementById('meme');
     if (memeData && memeData.memes && memeData.memes.length > 0) {
-        memeElement.src = memeData.memes[currentMemeIndex].url;
-        currentMemeIndex = (currentMemeIndex + 1) % memeData.memes.length;
+        const tempImage = new Image();
+        tempImage.onload = function () {
+            const width = tempImage.width;
+            const height = tempImage.height;
+
+            memeElement.src = memeData.memes[currentMemeIndex].url;
+            memeElement.width = width;
+            memeElement.height = height;
+
+            currentMemeIndex = (currentMemeIndex + 1) % memeData.memes.length;
+        };
+        tempImage.src = memeData.memes[currentMemeIndex].url;
     } else {
         console.error('Invalid meme data:', memeData);
     }
 }
 
-fetchMemeData();
 
-setInterval(updateMemePreview, 10000);
+
+document.addEventListener('DOMContentLoaded', function () {
+    particlesJS.load('particles-js', '/assets/js/particles.json', function () {
+    });
+    console.log("LOL");
+    console.log("LOLux");
+    fetchMemeData();
+    setInterval(updateMemePreview, 10000);
+});
+
+
+
+
 
